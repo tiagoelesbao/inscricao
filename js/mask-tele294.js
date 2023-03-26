848ac24d839e0941318b15b478ecbd5d9124700d
tiagoelesbao <tiago.e@outlook.com>
2023-03-25 21:36:57 -0300

    comit 3

--- js/mask-tele294.js
+++ js/mask-tele294.js
@@ -4,8 +4,6 @@ const testeFormField = document.getElementById("tel");
 const phoneInput = window.intlTelInput(phoneInputField, {
   preferredCountries: ["br", "us", "pt"],
   initialCountry: "br",
-  utilsScript:
-    "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
 });
 
 /*  MASCARA TELEFONE BRASIL */
